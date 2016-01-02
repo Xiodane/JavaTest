52a8efea4b911a2eaa1d70b09036740970de249d
Xiodane <Ilja098@mail.ru>
2016-01-03 00:16:17 +0200

    Second Commit

--- src/main/java/com/Ilya/TestOne/Main.java
+++
@@ -1,7 +0,0 @@
-package com.Ilya.TestOne;
-
-/**
- * Created by ComenT on 03.01.2016.
- */
-public class Main {
-}
---
+++ src/main/java/com/Ilya/TestOne/MainFirstClass.java
@@ -0,0 +1,10 @@
+package com.Ilya.TestOne;
+
+/**
+ * Created by ComenT on 03.01.2016.
+ */
+public class MainFirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
